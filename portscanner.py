1cdec3d382ffaf2218a6a1bf2decbb0e0fdffd9f
NickBwalley <nickbwalley@gmail.com>
2023-04-08 10:32:08 +0300

    last commit

--- portscanner.py
+++ portscanner.py
@@ -1,3 +1,25 @@
 import socket
 import termcolor
 
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print("[+] Port Opened " + str(port))
+    except: 
+        print("[-] Port Closed " + str(port))
+
+targets = input("[*] Enter Targets To Scan(split them by comma): ") #192.168.1.1, 192.168.1.5
+ports = input("[*] Enter How Many Port To Scan: ")
+if ',' in targets:
+    print("[*] Scanning Multiple Targets")
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
+else:
+    scan(targets, ports)
