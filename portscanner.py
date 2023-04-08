0fc324a5e6d9762e2039b74cdf74c910e6c4a466
NickBwalley <nickbwalley@gmail.com>
2023-04-08 10:56:21 +0300

    last commit

--- portscanner.py
+++ portscanner.py
@@ -3,23 +3,26 @@ import termcolor
 
 
 def scan(target, ports):
-    for port in range(1, ports):
-        scan_port(target, port)
+	print('\n' + ' Starting Scan For ' + str(target))
+	for port in range(1,ports):
+		scan_port(target,port)
 
 
 def scan_port(ipaddress, port):
-    try:
-        sock = socket.socket()
-        sock.connect((ipaddress, port))
-        print("[+] Port Opened " + str(port))
-    except: 
-        print("[-] Port Closed " + str(port))
+	try:
+		sock = socket.socket()
+		sock.connect((ipaddress, port))
+		print("[+] Port Opened " + str(port))
+		sock.close()
+	except:
+		pass
 
-targets = input("[*] Enter Targets To Scan(split them by comma): ") #192.168.1.1, 192.168.1.5
-ports = input("[*] Enter How Many Port To Scan: ")
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ") #192.168.1.1,192.168.1.5
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
 if ',' in targets:
-    print("[*] Scanning Multiple Targets")
-    for ip_addr in targets.split(','):
-        scan(ip_addr.strip(' '), ports)
+	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
 else:
-    scan(targets, ports)
+	scan(targets,ports)
